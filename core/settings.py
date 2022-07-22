8318d24f7e046f01d070c553b32bd4a1625d4ee9
MohammedMusaddiq <musaddiq838@gmail.com>
2022-07-22 14:31:43 +0530

    date 22 july update 2

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bai*gk4k+^oko8aw#+9u%7!%d8rfi*6r$iv@w0w=j(k6-(2)*w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
