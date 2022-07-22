2bb7094a4bed970419c4588b6b2a3b66f810bf98
MohammedMusaddiq <musaddiq838@gmail.com>
2022-07-22 13:50:32 +0530

    date 22 july update 2

--- core/settings.py
+++ core/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 WHITENOISE_USE_FINDERS = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
